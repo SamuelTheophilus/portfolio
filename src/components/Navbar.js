1e813133eb33e8fb85aeeb2cf05fa94d37827ef2
SamuelTheophilus <ansahsamueltheophilus@gmail.com>
2023-02-16 06:08:00 +0000

    second commit, using feature branches afterwards

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
   return (
     <nav className="w-full shadow-md bg-white justify-between h-20 sticky z-10 top-0 left-0">
       <div className="flex justify-between items-center w-full h-full px-5">
         <div className="">
-          <h1>Samuel Theophilus</h1>
+          <h1>SamuelTheophilus</h1>
         </div>
         <div className="">
           <ul className="hidden md:flex">
@@ -22,10 +23,12 @@ const Navbar = () => {
               <li  className="ml-10 text-sm  hover:border-b"> Skills</li>
             </Link>
           </ul>
+          <div className="md:hidden hover:cursor-pointer">
+            <AiOutlineMenu size={25}/>
+          </div>
         </div>
-
-
       </div>
+
     </nav>
   );
 }
